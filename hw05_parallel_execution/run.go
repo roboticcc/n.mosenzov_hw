58e46e10b88415266f0777dd02e567ad48ad62d2
nikita <cosmeanq@protonmail.com>
2025-03-11 18:53:30 +0600

    review adjustments

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -16,59 +16,41 @@ func Run(tasks []Task, n, m int) error {
 	if n <= 0 {
 		return fmt.Errorf("number of workers must be positive, got %d", n)
 	}
-	if len(tasks) == 0 {
+	if len(tasks) == 0 || m <= 0 {
 		return nil
 	}
 
 	tasksChan := make(chan Task)
-	errorsChan := make(chan struct{}, n)
-	termChan := make(chan struct{})
 	var errorCount int32
 	var wg sync.WaitGroup
 
 	wg.Add(n)
 	for i := 0; i < n; i++ {
-		go worker(tasksChan, errorsChan, termChan, &wg, &errorCount)
+		go worker(tasksChan, &wg, &errorCount)
 	}
 
-	go func() {
-		defer close(tasksChan)
-		for _, task := range tasks {
-			tasksChan <- task
-			if int(atomic.LoadInt32(&errorCount)) == m {
-				termChan <- struct{}{}
-			}
+	for _, task := range tasks {
+		if int(atomic.LoadInt32(&errorCount)) >= m {
+			break
 		}
-	}()
+		tasksChan <- task
+	}
+	close(tasksChan)
 
-	go func() {
-		wg.Wait()
-		close(errorsChan)
-	}()
-
-	for range errorsChan {
-		if m > 0 && int(atomic.LoadInt32(&errorCount)) > m {
-			return ErrErrorsLimitExceeded
-		}
+	wg.Wait()
+	if int(atomic.LoadInt32(&errorCount)) >= m {
+		return ErrErrorsLimitExceeded
 	}
 
 	return nil
 }
 
-func worker(tasksChan <-chan Task, errorsChan chan<- struct{},
-	termChan <-chan struct{}, wg *sync.WaitGroup, errorCount *int32,
-) {
+func worker(tasksChan <-chan Task, wg *sync.WaitGroup, errorCount *int32) {
 	defer wg.Done()
 
 	for task := range tasksChan {
 		if err := task(); err != nil {
 			atomic.AddInt32(errorCount, 1)
-			select {
-			case <-termChan:
-				break
-			case errorsChan <- struct{}{}:
-			default:
-			}
 		}
 	}
 }

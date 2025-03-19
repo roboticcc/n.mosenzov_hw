b1b4f04f39922d937db3f33853e03494bc62fce7
nikita <cosmeanq@protonmail.com>
2025-03-19 23:49:19 +0600

    test added

--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -91,6 +91,23 @@ func TestPipeline(t *testing.T) {
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
 	})
+
+	t.Run("empty input", func(t *testing.T) {
+		in := make(Bi)
+		go func() {
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		start := time.Now()
+		for s := range ExecutePipeline(in, nil, stages...) {
+			result = append(result, s.(string))
+		}
+		elapsed := time.Since(start)
+
+		require.Len(t, result, 0)
+		require.Less(t, int64(elapsed), int64(sleepPerStage)+int64(fault))
+	})
 }
 
 func TestAllStageStop(t *testing.T) {
